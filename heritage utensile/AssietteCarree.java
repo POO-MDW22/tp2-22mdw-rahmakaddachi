7cb4d2c7e1393a5f440c1ce896356c0a73590617
rahma kaddachi <rahmabenali17@gmail.com>
2023-11-30 21:30:16 +0100

    Add files via upload

---
+++ heritage utensile/AssietteCarree.java
@@ -0,0 +1,12 @@
+public class AssietteCarree extends Assiette{
+        private double cote;
+        public AssietteCarree (int anneefabrication,double cote) {
+            super(anneefabrication);
+            this.cote = cote;
+        }
+
+        public double getcote(){
+            return cote;
+        }
+    }
+
---
+++ heritage utensile/Cuillere.java
@@ -0,0 +1,12 @@
+public class Cuillere extends Ustensile{
+        private double Longueur;
+        public Cuillere(int anneefabrication,double Longueur) {
+            super(anneefabrication);
+            this.Longueur = Longueur;
+        }
+        public double getLongueur() {
+            return Longueur;
+        }
+
+    }
+
